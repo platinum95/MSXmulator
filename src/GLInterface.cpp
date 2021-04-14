279f9dcb1b1707bc802f8e4dfa9670cd7e910911
platinum95 <platinum95@gmail.com>
2021-04-13 18:39:17 -0700

    First-pass OpenGL output interface complete

--- src/GLInterface.cpp
+++ src/GLInterface.cpp
@@ -20,8 +20,8 @@ inline std::uint8_t operator ""_u( unsigned long long value ) {
 
 namespace {
 
-constexpr uint32_t RenderSurfaceWidth{ 284u * 2 };
-constexpr uint32_t RenderSurfaceHeight{ 243u * 2};
+constexpr uint32_t RenderSurfaceWidth{ 284u };
+constexpr uint32_t RenderSurfaceHeight{ 243u };
 constexpr uint32_t BytesPerPixel{ 3u };
 constexpr uint32_t FramebufferSize{ RenderSurfaceHeight * RenderSurfaceWidth * BytesPerPixel };
 
@@ -34,11 +34,24 @@ constexpr std::array<float, 2 * 3 * 2> Vertices = {
     -1.0f, 1.0f
 };
 
+constexpr std::array<float, 2 * 3 * 2> TextureCoords = {
+    0.0f, 1.0f,
+    1.0f, 1.0f,
+    1.0f, 0.0f,
+    0.0f, 1.0f,
+    1.0f, 0.0f,
+    0.0f, 0.0f
+};
+
 const char *VertexShaderSource = R"SHADER(
 #version 330 core
 layout ( location = 0 ) in vec2 vPos;
+layout ( location = 1 ) in vec2 tPosVS;
+
+out vec2 tPosFS;
 
 void main() {
+    tPosFS = tPosVS;
     gl_Position = vec4( vPos.x, vPos.y, 0.0, 1.0 );
 }
 )SHADER";
@@ -47,22 +60,28 @@ const char *FragmentShaderSource = R"SHADER(
 #version 330 core
 out vec4 FragColor;
 
+in vec2 tPosFS;
+
+uniform sampler2D frame;
+
 void main() {
-    FragColor = vec4( 1.0f, 0.5f, 0.2f, 1.0f );
+    FragColor = texture( frame, tPosFS );
 } 
 )SHADER";
 
 struct GLInterfaceState {
     bool initialised{ false };
-    GLFWwindow* window{ nullptr };
-    GLFWmonitor *monitor{ nullptr };
-    GLFWwindow *share{ nullptr };
-    std::array<uint8_t, FramebufferSize> framebuffer{ 0u };
 
-    uint32_t VBO;
-    uint32_t VAO;
-    uint32_t shader;
-    
+    GLFWwindow  *window{ nullptr };
+    GLFWmonitor *monitor{ nullptr };
+    GLFWwindow  *share{ nullptr };
+
+    std::array<GLuint, 2>   VBOs;
+    GLuint                  VAO;
+    GLuint                  shader;
+    GLuint                  texture;
+
+    std::array<uint8_t, FramebufferSize> framebuffer{ 0u };
 
 } state;
 
@@ -117,11 +136,23 @@ void Initialise( uint16_t width, uint16_t height ) {
     glGenVertexArrays( 1, &state.VAO );
     glBindVertexArray( state.VAO );
 
-    glGenBuffers( 1, &state.VBO );
-    glBindBuffer( GL_ARRAY_BUFFER, state.VBO );
+    glGenBuffers( static_cast<GLsizei>( state.VBOs.size() ), state.VBOs.data() );
+    glBindBuffer( GL_ARRAY_BUFFER, state.VBOs[ 0 ] );
     glBufferData( GL_ARRAY_BUFFER, sizeof( Vertices ), Vertices.data(), GL_STATIC_DRAW );
     glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof( float ), nullptr );
     glEnableVertexAttribArray( 0 );
+
+    glBindBuffer( GL_ARRAY_BUFFER, state.VBOs[ 1 ] );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( TextureCoords ), TextureCoords.data(), GL_STATIC_DRAW );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof( float ), nullptr );
+    glEnableVertexAttribArray( 1 );
+
+    glGenTextures( 1, &state.texture );
+    glBindTexture( GL_TEXTURE_2D, state.texture );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, RenderSurfaceWidth, RenderSurfaceHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, static_cast<void*>( state.framebuffer.data() ) );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
 
     auto CheckShaderCompilation = [] ( auto shaderId ) {
         int  success;
@@ -168,10 +199,12 @@ void Initialise( uint16_t width, uint16_t height ) {
 
 void Cleanup() {
     if ( state.initialised ) {
-        glfwDestroyWindow( state.window );
-        glDeleteBuffers( 1, &state.VBO );
+        glDeleteBuffers( static_cast<GLsizei>( state.VBOs.size() ), state.VBOs.data() );
         glDeleteVertexArrays( 1, &state.VAO );
         glDeleteProgram( state.shader );
+        glDeleteTextures( 1, &state.texture );
+
+        glfwDestroyWindow( state.window );
     }
 }
 
@@ -180,9 +213,9 @@ void SetPixel( uint32_t x, uint32_t y, Colour colour ) {
     const size_t pixelOffset = ( ( static_cast<size_t>( y ) * RenderSurfaceWidth ) + x ) * static_cast<size_t>( BytesPerPixel );
 
     // TODO - this could probably be done in a single block
-    state.framebuffer[ pixelOffset ] = std::get<0>( colourValue );
+    state.framebuffer[ pixelOffset + 0 ] = std::get<0>( colourValue );
     state.framebuffer[ pixelOffset + 1 ] = std::get<1>( colourValue );
-    state.framebuffer[ pixelOffset + 1 ] = std::get<2>( colourValue );
+    state.framebuffer[ pixelOffset + 2 ] = std::get<2>( colourValue );
 }
 
 void OutputFrame() {
@@ -191,9 +224,11 @@ void OutputFrame() {
     }
     glUseProgram( state.shader );
     glBindVertexArray( state.VAO );
-    glBindBuffer( GL_ARRAY_BUFFER, state.VBO );
+    glBindTexture( GL_TEXTURE_2D, state.texture );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, RenderSurfaceWidth, RenderSurfaceHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, static_cast<void*>( state.framebuffer.data() ) );
     glDrawArrays( GL_TRIANGLES, 0, 6 );
 
+    glfwPollEvents();
     glfwSwapBuffers( state.window );
 }
 
